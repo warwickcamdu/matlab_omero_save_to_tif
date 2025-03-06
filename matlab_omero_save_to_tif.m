48182ea9eeccfaf578d956f3a0b6ff021afe1770
Laura190 <l.cooper.5@warwick.ac.uk>
2025-03-06 16:12:41 +0000

    save using bioformats to include pixel resolution metadata

--- matlab_omero_save_to_tif.m
+++ matlab_omero_save_to_tif.m
@@ -12,72 +12,57 @@ client = loadOmero(servername);
 session = client.createSession(username, password);
 
 for i=1:length(imageIDs)
-[image_name,stacks]=fetch_stack(session,imageIDs(i));
-output_name=sprintf('%s.tif',image_name);
-save_as_imagej_tif(stacks, output_name);
+[image_name,stacks,metadata]=fetch_stack(session,imageIDs(i));
+output_name=sprintf('%s.ome.tif',image_name);
+save_as_ome_tif(stacks, output_name,metadata);
 end
 
 %Close session with OMERO
 client.closeSession();
 
-function save_as_imagej_tif(input, output_path)
-    %input is multidimensional uint16 array of image stack with dimensions in
-    %order sizeX,sizeY,sizeZ,sizeC,sizeT
-    %output_path is where to save image (e.g. use 'test.tif' to save in current
-    %direction
-    MultiDimImg=input;
-
-    fiji_descr = ['ImageJ=1.52p' newline ...
-        'images=' num2str(size(MultiDimImg,3)*...
-        size(MultiDimImg,4)*...
-        size(MultiDimImg,5)) newline...
-        'channels=' num2str(size(MultiDimImg,4)) newline...
-        'slices=' num2str(size(MultiDimImg,3)) newline...
-        'frames=' num2str(size(MultiDimImg,5)) newline...
-        'hyperstack=true' newline...
-        'mode=grayscale' newline...
-        'loop=false' newline...
-        'min=0.0' newline...
-        'max=65535.0'];  % change this to 256 if you use an 8bit image
-
-    t = Tiff(output_path,'w');
-    tagstruct.ImageLength = size(MultiDimImg,1);
-    tagstruct.ImageWidth = size(MultiDimImg,2);
-    tagstruct.Photometric = Tiff.Photometric.MinIsBlack;
-    tagstruct.BitsPerSample = 16;
-    tagstruct.SamplesPerPixel = 1;
-    tagstruct.Compression = Tiff.Compression.LZW;
-    tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
-    tagstruct.SampleFormat = Tiff.SampleFormat.UInt;
-    tagstruct.ImageDescription = fiji_descr;
-    for frame = 1:size(MultiDimImg,5)
-        for slice = 1:size(MultiDimImg,3)
-            for channel = 1:size(MultiDimImg,4)
-                t.setTag(tagstruct)
-                t.write(MultiDimImg(:,:,slice,channel,frame));
-                t.writeDirectory(); % saves a new page in the tiff file
-            end
-        end
-    end
-    t.close()
+function save_as_ome_tif(input, output_path, metadata)    
+bfsave(input, output_path, 'metadata', metadata);
 end
 
-function [image_name,stacks]=fetch_stack(session,imageID)
+function [image_name,stacks,metadata]=fetch_stack(session,imageID)
 %session is an omero session
 %imageID is the omero imageID to be downloaded and saved
     image = getImages(session,imageID);
     filename=image.getName().getValue();
     [~,image_name,~] = fileparts(string(filename));
+
     pixels = image.getPrimaryPixels();
+    % Get pixel resolution (voxel size in micrometers)
+    voxelX = pixels.getPhysicalSizeX().getValue();
+    voxelY = pixels.getPhysicalSizeY().getValue();
+    % Z-resolution might be missing in some datasets
+    voxelZ = NaN;  % Default to NaN if unavailable
+    if ~isempty(pixels.getPhysicalSizeZ())
+        voxelZ = pixels.getPhysicalSizeZ().getValue();
+    end
+
     sizeX = pixels.getSizeX().getValue(); % The number of pixels along the X-axis.
     sizeY = pixels.getSizeY().getValue(); % The number of pixels along the Y-axis.
     sizeZ = pixels.getSizeZ().getValue(); % The number of z-sections.
     sizeT = pixels.getSizeT().getValue(); % The number of timepoints.
     sizeC = pixels.getSizeC().getValue(); % The number of channels.
     stacks=zeros(sizeX,sizeY,sizeZ,sizeC,sizeT,'uint16');
-    for t=0:sizeT-1
-        for c=0:sizeC-1
-            stacks(:,:,:,c+1,t+1)=getStack(session, image, c, t);
+    [store, pixels] = getRawPixelsStore(session, image);
+    for z = 0 : sizeZ - 1
+        for c = 0 : sizeC - 1
+            for t = 0 : sizeT - 1
+            stacks(:,:,z+1,c+1,t+1) = getPlane(pixels, store, z, c, t);
+            end
         end
     end
+    store.close();
+    
+    metadata = createMinimalOMEXMLMetadata(stacks);
+    pixelSize = ome.units.quantity.Length(java.lang.Double(voxelX), ome.units.UNITS.MICROMETER);
+    metadata.setPixelsPhysicalSizeX(pixelSize, 0);
+    metadata.setPixelsPhysicalSizeY(pixelSize, 0);
+    pixelSizeZ = ome.units.quantity.Length(java.lang.Double(voxelZ), ome.units.UNITS.MICROMETER);
+    metadata.setPixelsPhysicalSizeZ(pixelSizeZ, 0);
+    pixelSizeZ = ome.units.quantity.Length(java.lang.Double(voxelZ), ome.units.UNITS.MICROMETER);
+
 end
